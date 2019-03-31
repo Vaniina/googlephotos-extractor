b863ef37f16c7c6ca605f1f962b431f81ef6681c
Vaniina <anna.vessereau@gmail.com>
2019-03-31 19:19:15 +0200

    Get back photo list

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const axios = require('axios');
 const {google} = require('googleapis');
 require('dotenv').config();
 
@@ -8,13 +9,57 @@ const oauth2Client = new google.auth.OAuth2(
     "http://localhost:" + process.env.SERVER_PORT
 );
 
-http.createServer((req, res) => {
+http.createServer(async (req, res) => {
+    let extracting = false;
+    const position = req.url.indexOf('?code=');
     const url = oauth2Client.generateAuthUrl({
         access_type: 'online',
         scope: "https://www.googleapis.com/auth/photoslibrary.readonly"
     });
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<a href="' + url + '"> extraire photos</a>');
+    if (position !== -1) {
+        const code = req.url.substr(position + 6);
+
+        try {
+            const {tokens} = await oauth2Client.getToken(code);
+            const photos = await getAllPhotos(tokens.access_token);
+
+            photos.forEach(async (photo) => {
+                await downloadPhoto(photo);
+            });
+
+            extracting = true;
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    res.writeHead(200,{'Content-Type': 'text/html'});
+
+    if (extracting) {
+        res.write('Récupération des photos...');
+    } else {
+        res.write('<a href="' + url + '">Extraire photos</a>');
+    }
+
     res.end();
 }).listen(process.env.SERVER_PORT);
+
+console.log("Starting server at port", process.env.SERVER_PORT);
+
+
+async function getAllPhotos (accessToken) {
+    const {data} = await axios({
+        method: "GET",
+        url: "https://photoslibrary.googleapis.com/v1/mediaItems?pageSize=100",
+        headers: {
+            Authorization: "Bearer " + accessToken
+        }
+    });
+
+    return data.mediaItems;
+}
+
+async function downloadPhoto (photo) {
+
+}
